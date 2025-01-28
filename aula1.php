59e6ab97d4398690c846c79fa025f2f8fc44b38a
LUHK <schuchlukas82@gmail.com>
2025-01-28 19:40:20 -0300

    Primeira alteração

---
+++ aula1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Word</h1>
+</body>
+</html>
+
+<?php 
+
+$message = "Hello word" . PHP_EOL . "hei\n" ;
+
+$alunos = ["Cleide", "Valdisnei", "Marcio"];
+
+echo $message;
+
+
+var_dump($alunos);
+
+if("22" === 22){
+    echo "monkey";
+}
+
+?>
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Modulo D atividades
+
+Este repositorio, são atividades feitas durante o modulo d do senac, feito para estudar php, java script, html e css
+tambem sera usado banco de dados, para fazer futuros projetos
