6c6f1ed2a8cb77da5b32cbb5f736743b1489a637
Luhk <schuchlukas82@gmail.com>
2025-03-13 21:57:52 -0300

    adicionando script

---
+++ AT7/app.js
@@ -0,0 +1,9 @@
+const nome = document.getElementById("#info-nome");
+const image = document.getElementById("#info-imagem");
+const status = document.getElementsByClassName(".info-status");
+const habilidades = document.getElementsByClassName(".info-habilit");
+const id = document.getElementById("#poke-id");
+
+function showInfo(){
+
+}
